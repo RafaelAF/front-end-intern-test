a96960d3cc3d7e0fa09397a4866a47d3b8699bd6
Antonio Rafael <andrade.rafa71@gmail.com>
2022-08-18 20:49:11 -0300

    Trabalhando na logica do formulario, validando a entrada de dados

--- main.js
+++ main.js
@@ -2,6 +2,19 @@
 // variáveis globais.
 (function() {
   // Elemento principal do formulário
+
+
+  //Expressão regular de email
+ 
+  const reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  // Função que valida email
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+          reg
+        );
+  };
   const contactForm = document.getElementById('contact');
 
   // Campos do formulário
@@ -12,19 +25,35 @@
   // "addEventListener" instrui o navegador a executar uma função sempre
   // que um evento específico acontece. Nesse caso, a função abaixo será
   // executada toda vez que o botão de envio for pressionado.
-  contactForm.addEventListener('submit', function() {
+  contactForm.addEventListener('submit', function(e) {
     // A propriedade "value" contém o valor atual de um input ou textarea
     const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+
 
     // A propriedade "length" contém o tamanho (em caracteres) de uma
     // uma string. Toda string contém essa propriedade.
     const nameLength = nameValue.length;
+    const emailLength = emailValue.length;
+    const messageLength = messageValue.length;
 
     // "window" é um objeto global que representa a janela (ou aba) do
     // navegador que está executando o código do seu site. O método
     // "alert" simplesmente mostra um aviso para o usuário contendo a
     // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
+
+
+    if((nameLength <= 2 || nameValue == null || nameValue == undefined)){
+      window.alert(`Por favor preencha seu nome`);
+    }else if((emailLength <= 2 || emailValue == null || emailValue == undefined)){
+      window.alert(`Por favor digite um  email`);
+
+    }else if(!(validateEmail(emailValue))){
+      window.alert('Digite um email valido')
+    }else if(messageLength < 20){
+      window.alert(`Insira pelo menos 20 caracteres na sua mensagem`);
+    }
 
     // Altere e complete essa função para validar os campos do formulário
     // de acordo com as especificações do teste. Boa sorte!
